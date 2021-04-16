e275239ebcfc47263263fd86e9df74497324607d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-04-16 13:43:29 +0300

    python/opject-server > dev: server

---
+++ packages/python/opject-server/opject_server/server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+
+
+server = Flask(__name__)
+
+
+
+@server.route('/request', methods=['POST'])
+def request():
+    response = {
+        "object": "",
+    }
+    return jsonify(response)
+
+
+@server.route('/register', methods=['POST'])
+def register():
+    response = {
+        "register": False,
+    }
+    return jsonify(response)
+
+
+@server.route('/check', methods=['POST'])
+def check():
+    response = {
+        "checked": False,
+    }
+    return jsonify(response)
+
+
+@server.route('/remove', methods=['POST'])
+def remove():
+    response = {
+        "removed": False,
+    }
+    return jsonify(response)
