6f8268f35e11d18866f949b378a3a47450d0a0d8
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-04-17 17:11:30 +0300

    python/opject-client > tests: files

---
+++ packages/python/opject-client/tests/simple.py
@@ -0,0 +1,37 @@
+# Run with
+# python -m tests.test_opject_client
+
+
+#region imports
+import unittest
+
+from opject_client import Client as OpjectClient
+#endregion imports
+
+
+
+#region module
+endpoint='http://localhost:7766'
+token='__TESTS__'
+
+
+class TestDelog(unittest.TestCase):
+    def test_simple(self):
+        opject_client = OpjectClient(
+            endpoint=endpoint,
+            token=token,
+        )
+        opject_client.register(
+            'SomeOpject',
+            'class SomeOpject:\n\tdef __init__(self):\n\t\tself.internal = 12\n\tdef read(self):\n\t\treturn self.internal\n',
+        )
+        some_opject = opject_client.require('SomeOpject')
+        print(some_opject.read())
+#endregion module
+
+
+
+#region runner
+if __name__ == '__main__':
+    unittest.main()
+#endregion runner
--- packages/python/opject-client/tests/test_opject_client.py
+++ packages/python/opject-client/tests/test_opject_client.py
@@ -1,10 +1,8 @@
 # Run with
-# python -m tests.test_opject_client
+# python -m tests.simple
 
 
 #region imports
-import unittest
-
 from opject_client import Client as OpjectClient
 #endregion imports
 
@@ -15,22 +13,22 @@ endpoint='http://localhost:7766'
 token='__TESTS__'
 
 
-class TestDelog(unittest.TestCase):
-    def test_simple(self):
-        opject_client = OpjectClient(
-            endpoint=endpoint,
-            token=token,
-        )
-        opject_client.register(
-            'SomeOpject',
-            'class SomeOpject:\n\tdef __init__(self):\n\t\tself.internal = 12\n\tdef read(self):\n\t\treturn self.internal\n',
-        )
-        some_opject = opject_client.require('SomeOpject')
-        print(some_opject.read())
+def test_simple():
+    opject_client = OpjectClient(
+        endpoint=endpoint,
+        token=token,
+    )
+    opject_client.register(
+        'SomeOpject',
+        'class SomeOpject:\n\tdef __init__(self):\n\t\tself.internal = 12\n\tdef read(self):\n\t\treturn self.internal\n',
+    )
+    some_opject = opject_client.require('SomeOpject')
+    print(some_opject.read())
 #endregion module
+
 
 
 #region runner
 if __name__ == '__main__':
-    unittest.main()
+    test_simple()
 #endregion runner
