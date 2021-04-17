5115e778b99cc30d03bd16b3f89a23cbc673ed57
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-04-17 19:59:48 +0300

    python/opject-server > dev: server

--- packages/python/opject-server/opject_server/server.py
+++ packages/python/opject-server/opject_server/server.py
@@ -1,4 +1,51 @@
 from flask import Flask, request, jsonify
+from flask_classful import FlaskView
+
+
+
+class EndpointRequire(FlaskView):
+    def post(self):
+        pass
+
+
+class Server:
+    def __init__(
+        self,
+        verify_token = None
+        get_object = None
+        get_metadata = None
+        register_object = None
+        register_metadata = None
+        remove_object = None
+    ):
+        self.custom_verify_token = verify_token
+        self.custom_get_object = get_object
+        self.custom_get_metadata = get_metadata
+        self.custom_register_object = register_object
+        self.custom_register_metadata = register_metadata
+        self.custom_remove_object = remove_object
+
+        self.app = Flask(__name__)
+        self.__handle_endpoints()
+        pass
+
+    def start(
+        self,
+        port,
+    ):
+        app.run()
+
+    def close(
+        self,
+    ):
+        app.run()
+
+
+    def __handle_endpoints(
+        self,
+    ):
+        pass
+
 
 
 server = Flask(__name__)
